d1021967789939592ac348e75564ee0c5e62c90f
VladislavKrs <vlad.14.vladfacel98@gmail.com>
2023-04-13 17:35:53 +0300

    added package.json

--- library-of-validation.js
+++ library-of-validation.js
@@ -1,4 +1,4 @@
-function validateLogin(loginVal, pattern, minLength, maxLength) {
+export function validateLogin(loginVal, pattern, minLength, maxLength) {
     if (loginVal.length <= minLength || loginVal.length >= maxLength) {
         return false;
     } else if (!loginVal.match(pattern) === true) {
@@ -8,7 +8,7 @@ function validateLogin(loginVal, pattern, minLength, maxLength) {
     }
 }
 
-function validateEmail(emailVal, pattern) {
+export function validateEmail(emailVal, pattern) {
     if (emailVal.match(pattern)) {
         return true
     } else {
@@ -16,7 +16,7 @@ function validateEmail(emailVal, pattern) {
     }
 }
 
-function validatePassword(passwordVal, pattern) {
+export function validatePassword(passwordVal, pattern) {
     if (pattern.test(passwordVal)) {
         return true;
     } else {
